38f9bfb02df0a9fc27c565f5d9c12a6a99c158c7
Gutierre Quaresma <108359160+GutQuaresma@users.noreply.github.com>
2025-08-20 18:26:12 +0000

    leigo d+

--- OPMATEMATICOS/INCRE_DRECRE.c
+++ OPMATEMATICOS/INCRE_DRECRE.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
 
 int main(){
-    
+    int numero1, resultado;
+
+    //Incremento 
+    //Pré-incremento
+    printf("Digite um número: ");
+    scanf("%d", &numero1);
+    printf("Antes do incremento: %d\n", numero1);
+    resultado = ++numero1;
+    printf("Após Pré-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+    //Pos-incremento
+    printf("Digite um número: ");
+    scanf("%d", &numero1);
+    printf("Antes do incremento: %d\n", numero1);
+
+    resultado = numero1++;
+    printf("Após Pós-incremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+    //Pré-decremento
+    printf("Digite um número: ");
+    scanf("%d", &numero1);
+    printf("Antes do decremento: %d\n", numero1);
+    resultado = --numero1;
+    printf("Após Pré-decremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+    //Decremento
+    //Pós-decremento
+    printf("Digite um número: ");
+    scanf("%d", &numero1);
+    printf("Antes do decremento: %d\n", numero1);
+    resultado = numero1--;
+    printf("Após Pós-decremento - Número 1: %d - Resultado: %d\n", numero1, resultado);
+
+    //Não entendi o funcionamento e sua utilização, mas está funcionando perfeitamente! :/
+
 
 }
