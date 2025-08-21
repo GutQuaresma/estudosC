756f48a6e1709ac9658f3106cfacaaf92946a7da
Gutierre Quaresma <108359160+GutQuaresma@users.noreply.github.com>
2025-08-21 18:40:47 +0000

    resolver problema

---
+++ ESTUDOS/ESTUDOS_3.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+
+int main(){
+    //Cadastro de nota do aluno + cálculo de média
+    int nota1, nota2, matricula, trab;
+    float media, notafinal;
+    char nome[50], turma;
+
+    printf("*** Bem-vindo(a) professor(a) ***");
+
+    //receber as informações básicas
+    printf("Escreva as informações básicas do aluno(a)!\n");
+    printf("Digite o nome do aluno: \n");
+    scanf(" %50[^\n]s", nome);
+
+    printf("Digite a turma do aluno(A-Z): \n");
+    scanf(" %c", &turma);
+
+    //printf("Digite o número da matrícula: \n");
+    //printf("%d", &matricula);
+
+    //receber as notas da prova
+    printf("** Cadastro das Notas **\n");
+
+    printf("Digite a nota da prova 1: \n");
+    scanf("%d", &nota1);
+
+    printf("Digite a nota da prova 2: \n");
+    scanf("%d", &nota2);
+
+    printf("Digite a nota do trabalho: \n");
+    scanf("%d", &trab);
+
+    //calculos da nota
+    media = (float)(nota1 + nota2) / 2;
+    notafinal = media + trab;
+
+    //exibição das informações
+    printf("Aluno(a): %s\n", nome);
+    printf("Matrícula: %d\n", matricula);
+    printf("Turma: %c\n", turma);
+    printf("Prova 1: %d\n", nota1);
+    printf("Prova 2: %d\n", nota2);
+    printf("Média: %.1f\n", media);
+    printf("Trabalho: %d\n", trab);
+    printf("Nota final: %.1f\n", notafinal);
+
+}
